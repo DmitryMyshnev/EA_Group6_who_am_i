8a677a7ae98fa1a538f6e836c9cc4e6610fcf436
Dmitry <dmitrymishnev@gmail.com>
2022-07-04 21:40:23 +0300

    added description to endpoints in swagger

--- src/main/java/com/eleks/academy/whoami/controller/GameController.java
+++ src/main/java/com/eleks/academy/whoami/controller/GameController.java
@@ -9,6 +9,7 @@ import com.eleks.academy.whoami.model.request.NewGameRequest;
 import com.eleks.academy.whoami.model.response.GameDetails;
 import com.eleks.academy.whoami.model.response.GameLight;
 import com.eleks.academy.whoami.service.GameService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.http.HttpStatus;
@@ -35,6 +36,9 @@ public class GameController {
 
     private final GameService gameService;
 
+    @Operation(summary = "To start Quick-game. After first time request create new game and will add first player with name from header." +
+            " After next request will add new player to game with name from  header." +
+            " Return info about new game.")
     @GetMapping("/quick-game")
     public ResponseEntity<GameDetails> quickGame(@RequestHeader(PLAYER) String player) {
         return this.gameService.quickGame(player)
@@ -42,6 +46,8 @@ public class GameController {
                 .orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
+    @Operation(summary = "To create new game and to add first player with name from header. " +
+            "Return info about new game")
     @PostMapping
     public ResponseEntity<GameDetails> createGame(@RequestHeader(PLAYER) String player,
                                                   @RequestBody NewGameRequest gameRequest) {
@@ -50,11 +56,14 @@ public class GameController {
                 .orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
+    @Operation(summary = "Find all existing game and game where has player with name from header. " +
+            " Return info about all games")
     @GetMapping
     public List<GameLight> findAvailableGames(@RequestHeader(PLAYER) String player) {
         return this.gameService.findAvailableGames(player);
     }
 
+    @Operation(summary = "Add new Player with name from header to game")
     @PostMapping("/{id}/players")
     public ResponseEntity<SynchronousPlayer> enrollToGame(@PathVariable("id") String id,
                                                           @RequestHeader(PLAYER) String player) {
@@ -63,6 +72,7 @@ public class GameController {
                 .orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
+    @Operation(summary = "Add character to player with name from header")
     @PostMapping("/{id}/characters")
     @ResponseStatus(HttpStatus.OK)
     public void suggestCharacter(@PathVariable("id") String id,
@@ -71,6 +81,7 @@ public class GameController {
         this.gameService.suggestCharacter(id, player, suggestion.getCharacter());
     }
 
+    @Operation(summary = "Find game by ID. Name in header does not matter. Return info about game")
     @GetMapping("/{id}")
     public ResponseEntity<GameDetails> findById(@PathVariable("id") String id,
                                                 @RequestHeader(PLAYER) String player) {
@@ -79,6 +90,7 @@ public class GameController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Rename  player in game. Header - old name. Query - new name. Return player with new name")
     @PostMapping("/{id}/players/name")
     public ResponseEntity<SynchronousPlayer> renamePlayer(@PathVariable("id") String id,
                                                           @RequestHeader(PLAYER) String player,
@@ -88,6 +100,7 @@ public class GameController {
                 .orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
+    @Operation(summary = "Start game. Available if status game is READY_TO_START. Name in header does not matter ")
     @PostMapping("/{id}")
     public ResponseEntity<GameDetails> startGame(@PathVariable("id") String id,
                                                  @RequestHeader(PLAYER) String player) {
@@ -96,6 +109,8 @@ public class GameController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = " To ask question by player with name from header. " +
+            "Available if status game is PROCESSING_QUESTION and status player is ASKING")
     @PostMapping("/{id}/questions")
     @ResponseStatus(HttpStatus.OK)
     public void askQuestion(@PathVariable("id") String id,
@@ -105,6 +120,7 @@ public class GameController {
         this.gameService.askQuestion(id, player, message.getMessage());
     }
 
+    @Operation(summary = "Leave player from game. Return info about game")
     @GetMapping("/{id}/leave-game")
     public ResponseEntity<GameDetails> leaveGame(@RequestHeader(PLAYER) String player, @PathVariable("id") String id) {
         return this.gameService.leaveGame(player, id)
@@ -112,6 +128,8 @@ public class GameController {
                 .orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
+    @Operation(summary = "Answer by player on question or guessing. " +
+            "Available if status game is PROCESSING_QUESTION and status player is ANSWERING")
     @PostMapping("/{id}/answer")
     @ResponseStatus(HttpStatus.OK)
     public void answerQuestion(@PathVariable("id") String id,
@@ -120,6 +138,8 @@ public class GameController {
         this.gameService.answerQuestion(id, player, message.getMessage());
     }
 
+    @Operation(summary = "Guess character by player. " +
+            "Available if status game is PROCESSING_QUESTION and status player is ASKING")
     @PostMapping("/{id}/guess")
     @ResponseStatus(HttpStatus.OK)
     public void guessingCharacter(@PathVariable("id") String id,
