4e1335139351bbb5c3b4f8beead12d7ced3c2153
Dmitry <dmitrymishnev@gmail.com>
2022-07-23 12:23:23 +0300

    added  permit for path /games/**

--- src/main/java/com/eleks/academy/whoami/configuration/SecurityConfig.java
+++ src/main/java/com/eleks/academy/whoami/configuration/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
+                .headers().frameOptions().sameOrigin().and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/auth/**",
@@ -50,7 +51,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/swagger-ui/**",
                         "/swagger-resources/**",
                         "/v2/api-docs",
-                        "/games/**").permitAll()
+                        "/games/**",
+                        "/h2/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
