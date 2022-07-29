56a090118f9417fae0ab9c7378ba096a07a2deab
Dmitry <dmitrymishnev@gmail.com>
2022-07-29 12:20:26 +0300

    fix bug: not restore password

--- src/main/java/com/eleks/academy/whoami/service/impl/UserServiceImpl.java
+++ src/main/java/com/eleks/academy/whoami/service/impl/UserServiceImpl.java
@@ -135,9 +135,6 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public void restorePassword(String newPassword, String confirmToken) {
-        if (!newPassword.equals(confirmToken)) {
-            throw new ChangePasswordException("Passwords do not match");
-        }
         var email = getEmailByToken(confirmToken);
         tokenRepository.findByToken(confirmToken)
                 .or(() -> {
